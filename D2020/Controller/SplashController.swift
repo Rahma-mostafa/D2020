d127d514da01821adbdcfe9da36293b37942041b
Rahma Mostafa <mrahma057@gmail.com>
2021-05-29 00:51:59 +0200

    Add splash screens

---
+++ D2020/Controller/SplashController.swift
@@ -0,0 +1,30 @@
+//
+//  splashController.swift
+//  D2020
+//
+//  Created by Macbook on 28/05/2021.
+//
+
+import UIKit
+
+class SplashController: UIViewController {
+
+    @IBOutlet weak var pageView: UIPageControl!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

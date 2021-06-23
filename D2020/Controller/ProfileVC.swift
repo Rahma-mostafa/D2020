d77a82162d64c9a00224b6720cef0ea478bba083
Rahma Mostafa <mrahma057@gmail.com>
2021-06-23 23:23:34 +0200

    Create profile screen design

---
+++ D2020/Controller/ProfileVC.swift
@@ -0,0 +1,34 @@
+//
+//  ProfileVC.swift
+//  D2020
+//
+//  Created by Macbook on 23/06/2021.
+//
+
+import UIKit
+
+class ProfileVC: UIViewController {
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        scrollView.contentSize = CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height + 500)
+
+     }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

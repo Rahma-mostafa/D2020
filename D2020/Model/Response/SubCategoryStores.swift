d868bf7231d1f62c25e4853ede8f7875308776b4
mohamed <mohamedmagdy.9080@gmail.com>
2021-07-12 16:38:54 +0200

    Fix Subcategory

--- D2020/Model/Response/SubCategoryStores.swift
+++ D2020/Model/Response/SubCategoryStores.swift
@@ -5,37 +5,76 @@
 
 import Foundation
 
-// MARK: - SubCategoryStores
+//// MARK: - SubCategoryStores
+//struct SubCategoryStores: Codable {
+//    let status: Bool
+//    let message: String
+//    let data: [SubCategoryStoresData]
+//}
+//
+//// MARK: - Datum
+//struct SubCategoryStoresData: Codable {
+//    let id: Int
+//    let name, code, arabicName: String
+//    let datumDescription, arabicDescription: String?
+////    let tags: JSONNull?
+//    let address: String
+//    let subscriberID, cityID, categoryID, subCategoryID: Int
+//    let phone: String
+//    let email: String?
+//    let image: String
+//    let video: String?
+//    let begin, end: String
+//    let status: Int
+//    let longi, lati: String
+//    let best, rating, views: Int
+//    let createdAt, updatedAt: String
+//
+//    enum CodingKeys: String, CodingKey {
+//        case id, name, code
+//        case arabicName = "arabic_name"
+//        case datumDescription = "description"
+//        case arabicDescription = "arabic_description"
+//        case address
+//        case subscriberID = "subscriber_id"
+//        case cityID = "city_id"
+//        case categoryID = "category_id"
+//        case subCategoryID = "sub_category_id"
+//        case phone, email, image, video, begin, end, status, longi, lati, best, rating, views
+//        case createdAt = "created_at"
+//        case updatedAt = "updated_at"
+//    }
+//}
 struct SubCategoryStores: Codable {
-    let status: Bool
-    let message: String
-    let data: [SubCategoryStoresData]
+    let status: Bool?
+    let message: String?
+    let data: [SubCategoryStoresData]?
 }
 
 // MARK: - Datum
 struct SubCategoryStoresData: Codable {
-    let id: Int
-    let name, code, arabicName: String
+    let id: Int?
+    let name, code, arabicName: String?
     let datumDescription, arabicDescription: String?
-    let tags: JSONNull?
-    let address: String
-    let subscriberID, cityID, categoryID, subCategoryID: Int
-    let phone: String
+    let address: String?
+    let subscriberID: Int?
+    let cityID, categoryID, subCategoryID: Int?
+    let phone: String?
     let email: String?
     let image: String
     let video: String?
-    let begin, end: String
-    let status: Int
-    let longi, lati: String
-    let best, rating, views: Int
-    let createdAt, updatedAt: String
+    let begin, end: String?
+    let status: Int?
+    let longi, lati: String?
+    let best, rating, views: Int?
+    let createdAt, updatedAt: String?
 
     enum CodingKeys: String, CodingKey {
         case id, name, code
         case arabicName = "arabic_name"
         case datumDescription = "description"
         case arabicDescription = "arabic_description"
-        case tags, address
+        case address
         case subscriberID = "subscriber_id"
         case cityID = "city_id"
         case categoryID = "category_id"
@@ -45,30 +84,3 @@ struct SubCategoryStoresData: Codable {
         case updatedAt = "updated_at"
     }
 }
-
-// MARK: - Encode/decode helpers
-
-//class JSONNull: Codable, Hashable {
-//
-//    public static func == (lhs: JSONNull, rhs: JSONNull) -> Bool {
-//        return true
-//    }
-//
-//    public var hashValue: Int {
-//        return 0
-//    }
-//
-//    public init() {}
-//
-//    public required init(from decoder: Decoder) throws {
-//        let container = try decoder.singleValueContainer()
-//        if !container.decodeNil() {
-//            throw DecodingError.typeMismatch(JSONNull.self, DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Wrong type for JSONNull"))
-//        }
-//    }
-//
-//    public func encode(to encoder: Encoder) throws {
-//        var container = encoder.singleValueContainer()
-//        try container.encodeNil()
-//    }
-//}

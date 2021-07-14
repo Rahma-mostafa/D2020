d3cf1f068f04c59595e324f50e7889e31f4c13db
Rahma Mostafa <mrahma057@gmail.com>
2021-07-14 16:22:31 +0200

    remove

--- D2020/Model/APIConstant.swift
+++ D2020/Model/APIConstant.swift
@@ -10,6 +10,4 @@ import Foundation
 enum APIConstant: String{
     case BASE_URL = "https://www.d2020.net/last_version/api/"
     case BASE_IMAGE_URL = "https://www.d2020.net/last_version/uploads/"
-    case BASE_STORE_URL = "user/category_stores/"
-    case BASE_DESCSTORE_URL = "user/desc_stores/"
 }
